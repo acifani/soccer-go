1f034d9c6ceb4e6b3e959a62da8659bf5176db45
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-02-20 22:42:46 +0100

    Disable travis tests temporarily

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,7 @@ export default {
   axiosConfig: {
     headers: {
       'X-Response-Control': 'full',
-      'X-Auth-Token':
-        process.env.SOCCER_CLI_API_KEY || '',
+      'X-Auth-Token': process.env.SOCCER_CLI_API_KEY || '',
     },
   },
   apiBaseUrl: 'http://football-data.org/v1/teams/',
