162a0a7585d1e399d4de08454e5860ceb59c7f8e
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-03-19 00:54:35 +0100

    Add league and team ascii titles

--- src/commands/fixtures.ts
+++ src/commands/fixtures.ts
@@ -1,10 +1,13 @@
+// tslint:disable-next-line
+const figlet = require('figlet');
 import { getLeagueByName } from '../constants/leagues';
 import { Fixture } from '../models';
 import * as api from './../api';
 
 export const printMatchday = async (leagueName: string) => {
   const league = getLeagueByName(leagueName);
-  console.log(league.name)
+  const leagueTitle = figlet.textSync(leagueName, { font: 'slant' });
+  console.log(leagueTitle);
   const fixturesData = await api.getMatchday(league.code);
   const table = Fixture.buildTable(fixturesData);
   console.log(table.toString());
