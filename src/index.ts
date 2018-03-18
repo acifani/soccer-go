b886a70f1bb4b03d8396752944e6784d2ef2d256
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-03-18 23:29:50 +0100

    Add update-notifier

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
+
 import * as inquirer from 'inquirer';
+// tslint:disable-next-line
+const pkg: UpdateNotifier.Package = require('../package.json');
+import * as UpdateNotifier from 'update-notifier';
 import * as commands from './commands';
 import { questions } from './constants/questions';
 
 (async () => {
+  const notifier = UpdateNotifier({ pkg });
+  notifier.notify();
+
   try {
     const answers: inquirer.Answers = await inquirer.prompt(questions);
     switch (answers.main) {
