a3c61f7b359272cfa08f1fe089a0358b7fe855cd
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-03-20 09:17:44 +0100

    Rename api token env variable

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ export default {
   apiBaseUrl: 'http://www.football-data.org/v1',
   axiosConfig: {
     headers: {
-      'X-Auth-Token': process.env.SOCCER_CLI_API_KEY || '',
+      'X-Auth-Token': process.env.SOCCER_GO_API_KEY || '',
       'X-Response-Control': 'full',
     },
   },
