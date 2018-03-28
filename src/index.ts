b6f42dd9b0c9a5b86d5a66dd16caad80209449e9
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-03-28 18:08:30 +0200

    Add commander and basic commands

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,12 @@
 import * as inquirer from 'inquirer';
 // tslint:disable-next-line
 const pkg: UpdateNotifier.Package = require('../package.json');
+import * as program from 'commander';
 import * as UpdateNotifier from 'update-notifier';
 import * as commands from './commands';
 import { questions } from './constants/questions';
 
-(async () => {
-  UpdateNotifier({ pkg }).notify();
-
+const askQuestions = async () => {
   try {
     const answers: inquirer.Answers = await inquirer.prompt(questions);
     switch (answers.main) {
@@ -30,4 +29,47 @@ import { questions } from './constants/questions';
   } catch (error) {
     console.log(error);
   }
+};
+
+(async () => {
+  UpdateNotifier({ pkg }).notify();
+
+  program.version(pkg.version);
+
+  program
+    .command('standings')
+    .alias('s')
+    .option('-l, --league <code>', 'league code')
+    .action(opts => commands.printStandings(opts.league));
+
+  program
+    .command('matchday')
+    .alias('m')
+    .option('-l, --league <code>', 'league code')
+    .action(opts => commands.printMatchday(opts.league));
+
+  program
+    .command('team <name>')
+    .alias('t')
+    .option('-l, --league <code>', 'league code')
+    .option('-f, --fixtures', 'display fixtures')
+    .option('-p, --players', 'display players')
+    .action((name, opts) =>
+      commands.printTeam(
+        name,
+        [opts.fixtures ? 'Fixtures' : '', opts.players ? 'Players' : ''],
+        opts.league
+      )
+    );
+
+  program.command('*').action(cmd => {
+    console.log(`Unknown command "${cmd}".`);
+    process.exit(1);
+  });
+
+  if (process.argv.length === 2) {
+    await askQuestions();
+  }
+
+  program.parse(process.argv);
 })();
