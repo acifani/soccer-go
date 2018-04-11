6b7d930468b481ed5cc9962bc326362dbdf7dac4
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-04-11 18:30:43 +0200

    Add command examples

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@
 import * as inquirer from 'inquirer';
 // tslint:disable-next-line
 const pkg = require('../package.json');
+import * as Chalk from 'chalk';
 import * as program from 'commander';
 import * as UpdateNotifier from 'update-notifier';
 import * as commands from './commands';
 import { getLeagueByName } from './constants/leagues';
 import { questions } from './constants/questions';
+const c = Chalk.default;
 
 const askQuestions = async () => {
   UpdateNotifier({ pkg }).notify({ isGlobal: true });
@@ -39,11 +41,27 @@ const askQuestions = async () => {
   program
     .command('standings <league>')
     .alias('s')
+    .on('--help', () =>
+      console.log(`
+        Get the full league table.
+
+        Example:
+
+        ${c.green('sgo s SA')}    Print Serie A table`)
+    )
     .action(league => commands.printStandings(league));
 
   program
     .command('matchday <league>')
     .alias('m')
+    .on('--help', () =>
+      console.log(`
+        Get matchday for a given League.
+
+        Example:
+
+        ${c.green('sgo m SA')}    Print Serie A matchday`)
+    )
     .action(league => commands.printMatchday(league));
 
   program
@@ -51,6 +69,15 @@ const askQuestions = async () => {
     .alias('t')
     .option('-f, --fixtures', 'include fixtures')
     .option('-p, --players', 'include players')
+    .on('--help', () =>
+      console.log(`
+        Get team games and/or players.
+
+        Example:
+
+        ${c.green('sgo t SA roma -f')}             Print AS Roma fixtures
+        ${c.green('sgo t PD "real madrid" -p')}    Print Real Madrid players`)
+    )
     .action((league, team, opts) =>
       commands.printTeam(
         team,
