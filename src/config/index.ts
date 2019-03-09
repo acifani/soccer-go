832c3c02480505a218d1649d93bb1c3df39fc975
Alessandro Cifani <alessandro.cifani@gmail.com>
2019-03-09 15:59:33 +0100

    Error on undefined api key env var

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ export default {
   apiBaseUrl: 'https://api.football-data.org/v2',
   axiosConfig: {
     headers: {
-      'X-Auth-Token': process.env.SOCCER_GO_API_KEY,
+      'X-Auth-Token': process.env.SOCCER_GO_API_KEY || apiKeyError(),
     },
   },
   cache: {
@@ -19,3 +19,19 @@ export default {
   },
   figletFont: 'Slant' as Fonts,
 };
+
+function apiKeyError() {
+  if (process.env.CI) {
+    return;
+  }
+
+  console.error(`
+  SOCCER_GO_API_KEY environment variable not set.
+
+      $ export SOCCER_GO_API_KEY=<football_data_api_key>
+
+  You can get your own API key over at
+  https://www.football-data.org/client/register
+  `);
+  process.exit(1);
+}
