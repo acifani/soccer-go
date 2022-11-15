8f6c60e132d170011550ca0269b12275c8316a33
Alessandro Cifani <alessandro.cifani@gmail.com>
2022-11-15 01:46:17 +0100

    refactor: fix lint errors

--- src/models/Fixture.ts
+++ src/models/Fixture.ts
@@ -32,78 +32,6 @@ const statusDisplayString: Record<Status, string> = {
   [Status.Penalty]: 'Penalties',
 };
 
-export interface ISide {
-  team: string;
-  goals: number;
-}
-
-export interface IFixtureLinks {
-  self: string;
-  homeTeam: string;
-  awayTeam: string;
-}
-
-export default class Fixture implements IRowable {
-  public id: number;
-  public home: ISide;
-  public away: ISide;
-  public matchday: number | null;
-  public stage: Stage;
-  public status: Status;
-  public date: Date;
-  public links: IFixtureLinks;
-
-  constructor(data: IFixtureJson) {
-    const [homeScore, awayScore] = this.getScores(data.score);
-
-    this.id = data.id;
-    this.home = {
-      goals: homeScore,
-      team: data.homeTeam.name,
-    };
-    this.away = {
-      goals: awayScore,
-      team: data.awayTeam.name,
-    };
-    this.matchday = data.matchday;
-    this.stage = data.stage;
-    this.status = data.status;
-    this.date = new Date(data.utcDate);
-    this.links = {
-      awayTeam: `${cfg.apiBaseUrl}/teams/${data.awayTeam.id}`,
-      homeTeam: `${cfg.apiBaseUrl}/teams/${data.homeTeam.id}`,
-      self: `${cfg.apiBaseUrl}/matches/${data.id}`,
-    };
-  }
-
-  public toRow = (): Table.Cell[] => [
-    `${this.home.team} - ${this.away.team}`,
-    [Status.InPlay, Status.Finished].includes(this.status)
-      ? pc.bold(`${this.home.goals} - ${this.away.goals}`)
-      : '',
-    statusDisplayString[this.status] ?? this.status,
-    dayjs(this.date).format('ddd, MMM D YYYY h:mm A'),
-    stageDisplayString[this.stage] ?? this.stage,
-  ];
-
-  private getScores(score: IFixtureJson['score']): [number, number] {
-    if (score.penalties?.home != null) {
-      return [score.penalties.home, score.penalties.away!];
-    }
-    if (score.extraTime?.home != null) {
-      return [score.extraTime.home, score.extraTime.away!];
-    }
-    if (score.fullTime.home != null) {
-      return [score.fullTime.home, score.fullTime.away!];
-    }
-    if (score.halfTime.home != null) {
-      return [score.halfTime.home, score.halfTime.away!];
-    }
-
-    return [0, 0];
-  }
-}
-
 export enum Stage {
   Final = 'FINAL',
   ThirdPlace = 'THIRD_PLACE',
@@ -163,6 +91,80 @@ export const stageDisplayString: Record<Stage, string> = {
   RELEGATION: 'Relegation',
   RELEGATION_ROUND: 'Relegation round',
 };
+
+export interface ISide {
+  team: string;
+  goals: number;
+}
+
+export interface IFixtureLinks {
+  self: string;
+  homeTeam: string;
+  awayTeam: string;
+}
+
+export default class Fixture implements IRowable {
+  public id: number;
+  public home: ISide;
+  public away: ISide;
+  public matchday: number | null;
+  public stage: Stage;
+  public status: Status;
+  public date: Date;
+  public links: IFixtureLinks;
+
+  constructor(data: IFixtureJson) {
+    const [homeScore, awayScore] = this.getScores(data.score);
+
+    this.id = data.id;
+    this.home = {
+      goals: homeScore,
+      team: data.homeTeam.name,
+    };
+    this.away = {
+      goals: awayScore,
+      team: data.awayTeam.name,
+    };
+    this.matchday = data.matchday;
+    this.stage = data.stage;
+    this.status = data.status;
+    this.date = new Date(data.utcDate);
+    this.links = {
+      awayTeam: `${cfg.apiBaseUrl}/teams/${data.awayTeam.id}`,
+      homeTeam: `${cfg.apiBaseUrl}/teams/${data.homeTeam.id}`,
+      self: `${cfg.apiBaseUrl}/matches/${data.id}`,
+    };
+  }
+
+  public toRow = (): Table.Cell[] => [
+    `${this.home.team} - ${this.away.team}`,
+    [Status.InPlay, Status.Finished].includes(this.status)
+      ? pc.bold(`${this.home.goals} - ${this.away.goals}`)
+      : '',
+    statusDisplayString[this.status] ?? this.status,
+    dayjs(this.date).format('ddd, MMM D YYYY h:mm A'),
+    stageDisplayString[this.stage] ?? this.stage,
+  ];
+
+  private getScores(score: IFixtureJson['score']): [number, number] {
+    /* eslint-disable @typescript-eslint/no-non-null-assertion */
+    if (score.penalties?.home != null) {
+      return [score.penalties.home, score.penalties.away!];
+    }
+    if (score.extraTime?.home != null) {
+      return [score.extraTime.home, score.extraTime.away!];
+    }
+    if (score.fullTime.home != null) {
+      return [score.fullTime.home, score.fullTime.away!];
+    }
+    if (score.halfTime.home != null) {
+      return [score.halfTime.home, score.halfTime.away!];
+    }
+    /* eslint-enable @typescript-eslint/no-non-null-assertion */
+
+    return [0, 0];
+  }
+}
 
 export interface IFixtureJson {
   id: number;
